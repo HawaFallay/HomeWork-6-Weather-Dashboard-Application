fc1003fd46fe1136219b7db77fa1e341b0227ac9
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-15 10:31:18 -0700

    script issues

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const { response } = require("express");
 
-var APIKey= ("7cd28cdc86752667ee0f9fca0d5ad25e")
-var searchButton = document.querySelector (".searchButton");
+var APIKey=("7cd28cdc86752667ee0f9fca0d5ad25e")
+var searchButton = document.querySelector(".searchButton");
 var inputBar = document.querySelector("#input");
 var column1 = document.querySelector(".column1");
 var cityNameAndDate = document.getElementById("cityName");
