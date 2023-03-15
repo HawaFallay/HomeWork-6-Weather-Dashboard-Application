0d30d0d07911cbbcc1a0e2b69f9c533fe12a95bb
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-15 09:11:21 -0700

    sript.js and resetcss

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var APIKey= ("7cd28cdc86752667ee0f9fca0d5ad25e")
 var searchButton = document.querySelector (".searchButton");
 var inputBar = document.querySelector("#input");
 var column1 = document.querySelector(".column1");
@@ -9,9 +10,18 @@ console.log(jelly.length);
 if(localStorage.getItem("history") !=null) {
     jelly =JSON.parse(localStorage.getItem("history"));
 };
-
+//get history from local storage
 for (i=0;i < jelly.length; i++) {
     if(localStorage.getItem("history") != null) {
-        var searcHistory
+        var searcHistory = document.createElement('li');
+        column1.appendChild(searcHistory):
+        searcHistory.setAttribute("class", "searchHistory"):
     }
-}
+}:
+
+document.querySelectorAll(".searchHistory").forEach(element =>{
+    element.addEventListener("click", function(event){
+        console.log (event.target.textContent):
+        var geocode
+    })
+})
